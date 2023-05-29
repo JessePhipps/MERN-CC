334d66bd10eace6b4cc771c6fed95c9a628b2762
Jesse Phipps <jesaiahphipps@gmail.com>
2023-05-29 12:51:20 -0500

    Post router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Container } from "react-bootstrap";
-import Header from "../components/Header";
-import HomeScreen from "../screens/HomeScreen";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+//import HomeScreen from "../screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <Header />
       <Container className="my-2">
-        <HomeScreen />
+        <Outlet />
       </Container>
     </>
   );
