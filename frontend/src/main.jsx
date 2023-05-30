0967e01d916d056907d12aa9532cee84eb540c7f
Jesse Phipps <jesaiahphipps@gmail.com>
2023-05-30 16:25:07 -0500

    Complete frontend/backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,13 +17,18 @@ import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
 import store from "./store";
 import { Provider } from "react-redux";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />} />
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/login' element={<LoginScreen />} />
-      <Route path="/register" element={<RegisterScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 );
